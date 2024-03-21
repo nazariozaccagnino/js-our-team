a01d72fcbd67f155cb1c35df5f09cdfc3def9658
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-21 14:52:42 +0100

    created the object array and inserted images

---
+++ script.js
@@ -0,0 +1,35 @@
+let team = [
+    {
+        fullname : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        image : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        fullname : 'Angela Caroll',
+        role : 'Chief Editor',
+        image : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        fullname : 'Walter Gordon',
+        role : 'Office Manager',
+        image : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        fullname : 'Angela Lopez',
+        role : 'Social Media Manager',
+        image : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        fullname : 'Scott Estrada',
+        role : 'Developer',
+        image : 'scott-estrada-developer.jpg'
+    },
+    {
+        fullname : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        image : 'barbara-ramos-graphic-designer.jpg'
+    },
+];
+for (let value of team){
+    console.log(value.fullname)
+}
